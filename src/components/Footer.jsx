517daabc523a769365da05c4d72281b6cf44a03a
tiagocreator <tiagoleite1405@gmail.com>
2022-06-08 19:50:36 +0000

    Add subscribe button to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="h-[150px] w-full mt-[50px] flex flex-col justify-center items-center text-[#fff] text-[1.2rem]">
+        <p>Subscribe to like your favorite movies</p>
+        <button className="border border-[#d4d4d4] hover:border-red-600 text-[#fff] hover:bg-red-600 py-[8px] px-[20px] mt-[16px]">Join Now</button>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
