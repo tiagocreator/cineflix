a96548e454db89409a1ee99cc9bc9120def9b81f
tiagocreator <tiagoleite1405@gmail.com>
2022-06-08 19:52:47 +0000

    Change Header position and font size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 const Header = () => {
   return (
-    <header className="w-full flex items-center justify-between py-[8px] px-[16px] md:px-[32px] z-[100] absolute">
-      <h1 className="text-red-600 text-[2.6rem] font-bold cursor-pointer">
+    <header className="w-full flex items-center justify-between py-[6px] px-[16px] md:px-[32px] z-[100]">
+      <h1 className="text-red-600 text-[2.4rem] font-bold cursor-pointer bg-[#000]">
         CINEFLIX
       </h1>
-      <nav className="text-[1.1rem]">
-        <button className="text-[#fff] ">Sign In</button>
-        <button className="bg-red-600 px-[24px] py-[8px] cursor-pointer font-bold text-[white] ml-[25px]">
-          Sign Out
+      <nav className="text-[1rem]">
+        <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">Sign In</button>
+        <button className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]">
+          Join Now
         </button>
       </nav>
     </header>
