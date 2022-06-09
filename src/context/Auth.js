5b54d158bbbbd61b74db60193bb771d841a89fe4
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-09 01:54:28 -0300

    Create Auth.js

---
+++ src/context/Auth.js
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useContext, useState } from 'react';
+import { auth } from '../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+const Auth = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function signIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  return <Auth.Provider value={{ signUp, user }}>{children}</Auth.Provider>;
+}
+
+export function UserAuth() {
+  return useContext(Auth);
+}
