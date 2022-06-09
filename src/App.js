c382589bb2c59e8cf617690557d5ae1f5e27e302
tiagocreator <tiagoleite1405@gmail.com>
2022-06-09 21:08:28 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/Auth';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Home from './pages/Home';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Account from './pages/Account';
 
 function App() {
   return (
     <>
+    <AuthProvider>
       <Header />
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/login" element={<LogIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/account" element={<Account/>} />
       </Routes>
       <Footer />
+    </AuthProvider>
     </>
   );
 }
