86cc6a696a4140fb8e2cee464314f930b3e831e4
tiagocreator <tiagoleite1405@gmail.com>
2022-06-09 21:10:00 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,28 @@
-import Main from '../components/Main';
+import Intro from '../components/Intro';
 import Row from '../components/Row';
 import Requests from '../Requests.js';
 
 const Home = () => {
   return (
     <>
-      <Main />
-      <Row id="popular" title="Popular" fetchURL={Requests.reqPopular} />
-      <Row id="top-rated" title="Top Rated" fetchURL={Requests.reqTop} />
-      <Row id="trending" title="Trending" fetchURL={Requests.reqTrend} />
-      <Row id="action" title="Action" fetchURL={Requests.reqAction} />
-      <Row id="adventure" title="Adventure" fetchURL={Requests.reqAdventure} />
-      <Row id="comedy" title="Comedy" fetchURL={Requests.reqComedy} />
-      <Row id="horror" title="Horror" fetchURL={Requests.reqHorror} />
-      <Row id="romance" title="Romance" fetchURL={Requests.reqRomance} />
-      <Row
-        id="documentary"
-        title="Documentary"
-        fetchURL={Requests.reqDocumentary}
-      />
+      <main>
+        <Intro />
+        <section>
+        <Row id="popular" title="Popular" fetchURL={Requests.reqPopular} />
+        <Row id="top-rated" title="Top Rated" fetchURL={Requests.reqTop} />
+        <Row id="trending" title="Trending" fetchURL={Requests.reqTrend} />
+        <Row id="action" title="Action" fetchURL={Requests.reqAction} />
+        <Row id="adventure" title="Adventure" fetchURL={Requests.reqAdventure} />
+        <Row id="comedy" title="Comedy" fetchURL={Requests.reqComedy} />
+        <Row id="horror" title="Horror" fetchURL={Requests.reqHorror} />
+        <Row id="romance" title="Romance" fetchURL={Requests.reqRomance} />
+        <Row
+          id="documentary"
+          title="Documentary"
+          fetchURL={Requests.reqDocumentary}
+        />
+        </section>
+        </main>
     </>
   );
 };
