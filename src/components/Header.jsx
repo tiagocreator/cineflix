033a26b23db9d682102dceff433f87667846481b
tiagocreator <tiagoleite1405@gmail.com>
2022-06-09 21:08:57 +0000

    Add Links to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="w-full flex items-center justify-between py-[5px] px-[16px] md:px-[32px] z-[100]">
-      <h1 className="text-red-600 text-[2.3rem] font-bold cursor-pointer bg-[#000]">
-        CINEFLIX
-      </h1>
+      <Link to='/'>
+        <h1 className="text-red-600 text-[2.3rem] font-bold cursor-pointer bg-[#000]">
+          CINEFLIX
+        </h1>
+      </Link>
       <nav className="text-[1rem]">
-        <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">Sign In</button>
-        <button className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]">
-          Join Now
-        </button>
+        <Link to='/login'>
+          <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">Sign In</button>
+        </Link>
+        <Link to='/signup'>
+          <button className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]">
+            Join Now
+          </button>
+        </Link>
       </nav>
     </header>
   );
