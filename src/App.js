072a8570e80338ee546be5bbf46fc841f2ef6faa
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-09 01:56:42 -0300

    Add a import line

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/Auth';
 import Home from './pages/Home';
 
 function App() {
