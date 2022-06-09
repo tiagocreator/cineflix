b04dfb93cad2f912e2d7026222643757747bc2be
tiagocreator <tiagoleite1405@gmail.com>
2022-06-09 21:08:09 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="h-[150px] w-full mt-[50px] flex flex-col justify-center items-center text-[#fff] text-[1.2rem]">
-        <p>Subscribe to like your favorite movies</p>
-        <button className="border border-[#d4d4d4] hover:border-red-600 text-[#fff] hover:bg-red-600 py-[8px] px-[20px] mt-[16px]">Join Now</button>
+      <div className="h-[80px] w-full flex flex-col justify-center items-center text-[#fff] text-[1rem]">
+        <p>All image rights belong to Netflix</p>
+        <p>Data taken from the TMDB api</p>
       </div>
     </footer>
   )
