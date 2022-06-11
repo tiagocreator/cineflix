9351354c94ea66024a392ce291dead4ef106720f
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-11 19:51:40 -0300

    Fix minor errors and inconsistencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,21 +8,25 @@ const Home = () => {
       <main>
         <Intro />
         <section>
-        <Row id="popular" title="Popular" fetchURL={Requests.reqPopular} />
-        <Row id="top-rated" title="Top Rated" fetchURL={Requests.reqTop} />
-        <Row id="trending" title="Trending" fetchURL={Requests.reqTrend} />
-        <Row id="action" title="Action" fetchURL={Requests.reqAction} />
-        <Row id="adventure" title="Adventure" fetchURL={Requests.reqAdventure} />
-        <Row id="comedy" title="Comedy" fetchURL={Requests.reqComedy} />
-        <Row id="horror" title="Horror" fetchURL={Requests.reqHorror} />
-        <Row id="romance" title="Romance" fetchURL={Requests.reqRomance} />
-        <Row
-          id="documentary"
-          title="Documentary"
-          fetchURL={Requests.reqDocumentary}
-        />
+          <Row id="popular" title="Popular" fetchURL={Requests.reqPopular} />
+          <Row id="top-rated" title="Top Rated" fetchURL={Requests.reqTop} />
+          <Row id="trending" title="Trending" fetchURL={Requests.reqTrend} />
+          <Row id="action" title="Action" fetchURL={Requests.reqAction} />
+          <Row
+            id="adventure"
+            title="Adventure"
+            fetchURL={Requests.reqAdventure}
+          />
+          <Row id="comedy" title="Comedy" fetchURL={Requests.reqComedy} />
+          <Row id="horror" title="Horror" fetchURL={Requests.reqHorror} />
+          <Row id="romance" title="Romance" fetchURL={Requests.reqRomance} />
+          <Row
+            id="documentary"
+            title="Documentary"
+            fetchURL={Requests.reqDocumentary}
+          />
         </section>
-        </main>
+      </main>
     </>
   );
 };
