a14f9f9d6aae491e83c1f2a6039eca8b63993b49
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-11 19:46:20 -0300

    Change "Heart" icon position

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const Movie = ({ item }) => {
   const [like, setLike] = useState(false);
   return (
-    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-[8px]">
+    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative hover:scale-105 mx-[10px] first:ml-[0] last:mr-[0]">
       <img
         className="w-full h-auto block"
         src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
@@ -16,9 +16,9 @@ const Movie = ({ item }) => {
         </p>
         <p>
           {like ? (
-            <FaHeart className="absolute top-[16px] left-[16px] text-[#d4d4d4]" />
+            <FaHeart className="absolute top-[16px] right-[16px] text-[#d4d4d4] text-[1.2rem]" />
           ) : (
-            <FaRegHeart className="absolute top-[16px] left-[16px] text-[#d4d4d4]" />
+            <FaRegHeart className="absolute top-[16px] right-[16px] text-[#d4d4d4] text-[1.2rem]" />
           )}
         </p>
       </div>
