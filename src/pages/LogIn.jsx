cf1cc9a7cecc372035ddcb1f74f351af73aea520
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-12 23:34:10 -0300

    Update LogIn.jsx

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,26 @@
 import BgImg from '../assets/img/signup.jpg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { UserAuth } from '../context/Auth';
 
 const LogIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { user, LogIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await LogIn(email, password);
+      navigate('/');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center w-full h-screen text-[#fff]">
       <img
@@ -14,14 +33,28 @@ const LogIn = () => {
         <div className="max-w-[450px] h-[450px] mx-auto bg-black/80 text-[#fff] ">
           <div className="max-w-[320px] mx-auto py-[45px]">
             <h1 className="text-[1.8rem] font-bold">Sign In</h1>
-            <form className="w-full flex flex-col py-[16px]">
+            {error ? (
+              <p className="bg-red-400 py-[8px] px-[10px] rounded">
+                {error.split('Firebase: ')}
+              </p>
+            ) : null}
+            <form
+              onSubmit={handleSubmit}
+              className="w-full flex flex-col py-[16px]"
+            >
               <input
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
                 className="p-[12px] my-[8px] bg-gray-800 rounded outline-none"
                 type="email"
                 placeholder="Email"
                 autoComplete="email"
               />
               <input
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
                 className="p-[12px] my-[8px] bg-gray-800 rounded outline-none"
                 type="password"
                 placeholder="Password"
