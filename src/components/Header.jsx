51048eb1680a5a4cd44c96f1c997c9588a7ad795
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-12 23:33:47 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/Auth';
 
 const Header = () => {
+  const { user, LogOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await LogOut();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header className="w-full flex items-center justify-between py-[5px] px-[16px] md:px-[32px] z-[100]">
       <Link to="/">
@@ -8,18 +21,34 @@ const Header = () => {
           CINEFLIX
         </h1>
       </Link>
-      <nav className="text-[1rem]">
-        <Link to="/login">
-          <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">
-            Sign In
+      {user?.email ? (
+        <nav className="text-[1rem]">
+          <Link to="/account">
+            <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">
+              Account
+            </button>
+          </Link>
+          <button
+            onClick={handleLogOut}
+            className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]"
+          >
+            Sign Out
           </button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]">
-            Join Now
-          </button>
-        </Link>
-      </nav>
+        </nav>
+      ) : (
+        <nav className="text-[1rem]">
+          <Link to="/login">
+            <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">
+              Sign In
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]">
+              Join Now
+            </button>
+          </Link>
+        </nav>
+      )}
     </header>
   );
 };
