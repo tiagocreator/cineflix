15095e2c0a02b2780882d9786e1ad07377661a7c
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-18 04:06:02 -0300

    Create Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,23 @@
+import BgImg from '../assets/img/signup.jpg';
+import SavedShows from '../components/SavedShows';
+
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <section>
+      <div className="relative flex w-full h-full text-[#fff]">
+        <img
+          className="absolute w-full h-[150px] sm:h-[250px] md:h-[300px] object-cover"
+          src={BgImg}
+          alt=""
+        />
+        <div className="bg-black/60 w-full h-[150px] sm:h-[250px] md:h-[300px] z-10"></div>
+        <div className="h-[150px] sm:h-[250px] md:h-[300px] left-[5%] absolute flex justify-center items-center z-20">
+          <h1 className="text-[1.8rem] md:text-[3rem] font-bold">My Account</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </section>
+  );
 };
 
 export default Account;
