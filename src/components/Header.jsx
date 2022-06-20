ca30afe9e90e0c3006228c465e32ac45caf92a5c
Tiago Leite <tiagoleite1405@gmail.com>
2022-06-20 19:57:39 -0300

    Change buttons padding and add hover effects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,18 +17,18 @@ const Header = () => {
   return (
     <header className="w-full flex items-center justify-between py-[5px] px-[16px] md:px-[32px] z-[100] h-[70px]">
       <Link to="/">
-        <img className="h-[45px] md:h-[55px]" src={Logo} alt="cineflix logo" />
+        <img className="h-[40px] md:h-[53px]" src={Logo} alt="cineflix logo" />
       </Link>
       {user?.email ? (
         <nav className="text-[1rem]">
           <Link to="/account">
-            <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">
+            <button className="border hover:bg-[#2D2D2D] active:bg-[#2D2D2D] border-[#d4d4d4] text-[#fff] py-[3px] md:py-[7px]  px-[10px] md:px-[18px]">
               Account
             </button>
           </Link>
           <button
             onClick={handleLogOut}
-            className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]"
+            className="bg-red-600 active:bg-red-700 py-[4px] md:py-[8px] px-[10px] md:px-[18px] cursor-pointer font-bold text-[#fff] ml-[11px] md:ml-[25px]"
           >
             Sign Out
           </button>
@@ -36,12 +36,12 @@ const Header = () => {
       ) : (
         <nav className="text-[1rem]">
           <Link to="/login">
-            <button className="border border-[#d4d4d4] text-[#fff] py-[7px] px-[18px]">
+            <button className="border hover:bg-[#2D2D2D] active:bg-[#2D2D2D] border-[#d4d4d4] text-[#fff] py-[3px] md:py-[7px]  px-[10px] md:px-[18px]">
               Sign In
             </button>
           </Link>
           <Link to="/signup">
-            <button className="bg-red-600 py-[7px] px-[18px] cursor-pointer font-bold text-[white] ml-[25px]">
+            <button className="bg-red-600 active:bg-red-700 py-[4px] md:py-[8px] px-[10px] md:px-[18px] cursor-pointer font-bold text-[white] ml-[11px] md:ml-[25px]">
               Join Now
             </button>
           </Link>
