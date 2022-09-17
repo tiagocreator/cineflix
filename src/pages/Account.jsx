bca5906517fc5f08a032c1e8df24f22261e39da7
tiagocreator <tiagoleite1405@gmail.com>
2022-09-17 01:05:59 +0000

    Refactor tailwindcss class names

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -4,7 +4,7 @@ import SavedShows from '../components/SavedShows';
 const Account = () => {
   return (
     <section>
-      <div className="relative flex w-full h-full text-[#fff]">
+      <div className="relative flex w-full h-full">
         <img
           className="absolute w-full h-[150px] sm:h-[250px] md:h-[300px] object-cover"
           src={BgImg}
@@ -12,7 +12,7 @@ const Account = () => {
         />
         <div className="bg-black/60 w-full h-[150px] sm:h-[250px] md:h-[300px] z-10"></div>
         <div className="h-[150px] sm:h-[250px] md:h-[300px] left-[5%] absolute flex justify-center items-center z-20">
-          <h1 className="text-[1.8rem] md:text-[3rem] font-bold">My Account</h1>
+          <h1 className="text-3xl md:text-5xl font-bold">My Account</h1>
         </div>
       </div>
       <SavedShows />
