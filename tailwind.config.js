56cb28889a8b2d856bfad4023e24f2dbd68dded1
tiagocreator <tiagoleite1405@gmail.com>
2022-09-17 01:00:14 +0000

    Add custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'theme-black': '#141416',
+      'theme-darkgray': '#2d2d2d',
+      'theme-lightgray': '#d4d4d4',
+      'theme-red': '#df0000',
+      'theme-darkred': '#cc0202', 
+      'theme-textgray': '#b4b4b4',
+    },
     extend: {},
   },
   plugins: [require('tailwind-scrollbar-hide')],
