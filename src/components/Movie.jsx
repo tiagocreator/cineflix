c7af28a396a74ed6dfad058b0bbd0bbf37b18a74
tiagocreator <tiagoleite1405@gmail.com>
2022-09-17 01:04:28 +0000

    Refactor tailwindcss class names

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,10 +5,12 @@ import { db } from '../firebase';
 import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
 
 const Movie = ({ item }) => {
+  
   const [like, setLike] = useState(false);
   const [saved, setSaved] = useState(false);
   const { user } = UserAuth();
-
+  
+  const heartStyle = `absolute top-4 right-4 ${like ? 'text-theme-red' : 'text-white'} text-xl`
   const movieId = doc(db, 'users', `${user?.email}`);
 
   const saveShow = async () => {
@@ -28,21 +30,21 @@ const Movie = ({ item }) => {
   };
 
   return (
-    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative hover:scale-105 mx-[10px] first:ml-[0] last:mr-[0]">
+    <div className="w-40 sm:w-[200px] md:w-60 lg:w-[280px] inline-block cursor-pointer relative hover:scale-105 mx-2.5 first:ml-0 last:mr-0">
       <img
         className="w-full h-auto block"
         src={`https://image.tmdb.org/t/p/w300/${item?.backdrop_path}`}
         alt={item?.title}
       />
-      <div className="absolute top-0 left-0 h-full w-full hover:bg-black/75 opacity-0 hover:opacity-100 text-[#fff]">
-        <p className="whitespace-normal text-[0.75rem] md:text-[0.87rem] font-bold flex justify-center items-center h-full text-center mx-[20%]">
+      <div className="absolute top-0 left-0 h-full w-full hover:bg-black/75 opacity-0 hover:opacity-100">
+        <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center mx-[20%]">
           {item.title}
         </p>
         <p onClick={saveShow}>
           {like ? (
-            <FaHeart className="absolute top-[16px] right-[16px] text-[#d4d4d4] text-[1.2rem]" />
+            <FaHeart className={heartStyle} />
           ) : (
-            <FaRegHeart className="absolute top-[16px] right-[16px] text-[#d4d4d4] text-[1.2rem]" />
+            <FaRegHeart className={heartStyle} />
           )}
         </p>
       </div>
