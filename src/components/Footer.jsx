36d4352502982f83e945bda1ffb9d464dfe9cd56
tiagocreator <tiagoleite1405@gmail.com>
2022-09-17 01:03:37 +0000

    Refactor tailwindcss class names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="h-[80px] w-full flex flex-col justify-center items-center text-[#fff] text-[1rem]">
+      <div className="h-20 w-full flex flex-col justify-center items-center text-base">
         <p>All image rights belong to Netflix</p>
         <p>Data taken from the TMDB api</p>
       </div>
