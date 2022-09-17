e968a6f19211ad36c722379c93628fe12501b656
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-17 10:49:34 -0300

    Refactor tailwindcss class names

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -9,6 +9,8 @@ const SignUp = () => {
   const { user, SignUp } = UserAuth();
   const navigate = useNavigate();
 
+  const inputStyle = 'p-3 my-2 rounded outline-none text-theme-black';
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -20,26 +22,23 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex justify-center items-center w-full h-screen text-[#fff]">
+    <div className="flex justify-center items-center w-full h-screen">
       <img
         className="hidden sm:block absolute w-full h-full object-cover"
         src={BgImg}
         alt=""
       />
-      <div className="bg-black/60 w-full h-full z-10"></div>
+      <div className="bg-theme-background-60 w-full h-full z-10"></div>
       <div className="absolute w-full z-20">
-        <div className="max-w-[450px] h-auto mx-auto bg-black/80 text-[#fff] ">
-          <div className="max-w-[320px] mx-auto py-[45px]">
-            <h1 className="text-[1.8rem] font-bold">Sign Up</h1>
-            <form
-              onSubmit={handleSubmit}
-              className="w-full flex flex-col py-[16px]"
-            >
+        <div className="max-w-[450px] h-auto mx-auto bg-theme-background-80">
+          <div className="max-w-[320px] mx-auto py-11">
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
-                className="p-[12px] my-[8px] bg-gray-800 rounded outline-none"
+                className={inputStyle}
                 type="email"
                 placeholder="Email"
                 autoComplete="email"
@@ -49,25 +48,27 @@ const SignUp = () => {
                 onChange={(e) => {
                   setPassword(e.target.value);
                 }}
-                className="p-[12px] my-[8px] bg-gray-800 rounded outline-none"
+                className={inputStyle}
                 type="password"
                 placeholder="Password"
                 autoComplete="current-password"
                 required
               />
-              <button className="bg-red-600 py-[12px] my-[25px] rounded font-bold">
+              <button className="bg-theme-red hover:bg-theme-darkred focus:bg-theme-darkred py-3 my-6 rounded font-bold">
                 Sign Up
               </button>
-              <div className="flex justify-between items-center text-[0.9rem] text-gray-500">
+              <div className="flex justify-between items-center text-sm text-theme-lightgray">
                 <p>
-                  <input className="mr-[8px]" type="checkbox" />
+                  <input className="mr-2" type="checkbox" />
                   Remember Me?
                 </p>
-                <p className="cursor-pointer">Need Help?</p>
+                <a className="cursor-pointer" href="/signup">
+                  Need Help?
+                </a>
               </div>
               <div>
-                <p className="mt-[16px]">
-                  <span className="text-gray-500 mr-[8px]">
+                <p className="mt-4">
+                  <span className="text-theme-lightgray mr-2">
                     Already subscribed to Cineflix?
                   </span>{' '}
                   <Link to="/login">Sign In</Link>
