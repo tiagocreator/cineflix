190ecefb5fb036e831eaedfa63850a627a28725d
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-17 10:49:22 -0300

    Refactor tailwindcss class names

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -10,7 +10,7 @@ const LogIn = () => {
   const { user, LogIn } = UserAuth();
   const navigate = useNavigate();
 
-  const inputClass = 'p-3 my-2 rounded outline-none text-theme-black'
+  const inputStyle = 'p-3 my-2 rounded outline-none text-theme-black';
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,16 +26,18 @@ const LogIn = () => {
   const handleError = () => {
     return error ? (
       <p className="bg-red-400 py-1.5 px-2.5 rounded">
-        {
-          error.includes('too-many-requests') ? 'Access to this account has been temporarily disabled due to many failed login attempts. You can try again later'
-          : error.includes('user-not-found') ? 'User not found'
-          : error.includes('invalid-email') ? 'Incorrect email adress' 
-          : error.includes('wrong-password') ? 'Incorrect password' 
-          : 'Invalid email adress or password'
-        }
+        {error.includes('too-many-requests')
+          ? 'Access to this account has been temporarily disabled due to many failed login attempts. You can try again later'
+          : error.includes('user-not-found')
+          ? 'User not found'
+          : error.includes('invalid-email')
+          ? 'Incorrect email adress'
+          : error.includes('wrong-password')
+          ? 'Incorrect password'
+          : 'Invalid email adress or password'}
       </p>
-    ) : null
-  }
+    ) : null;
+  };
 
   return (
     <div className="flex justify-center items-center w-full h-screen">
@@ -44,21 +46,18 @@ const LogIn = () => {
         src={BgImg}
         alt=""
       />
-      <div className="bg-black/60 w-full h-full z-10"></div>
+      <div className="bg-theme-background-60 w-full h-full z-10"></div>
       <div className="absolute w-full z-20">
-        <div className="max-w-[450px] h-[450px] mx-auto bg-black/80">
+        <div className="max-w-[450px] h-[450px] mx-auto bg-theme-background-80">
           <div className="max-w-[320px] mx-auto py-11">
             <h1 className="text-3xl font-bold">Sign In</h1>
-              {handleError()}
-            <form
-              onSubmit={handleSubmit}
-              className="w-full flex flex-col py-4"
-            >
+            {handleError()}
+            <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
-                className={inputClass}
+                className={inputStyle}
                 type="email"
                 placeholder="Email"
                 autoComplete="email"
@@ -67,24 +66,24 @@ const LogIn = () => {
                 onChange={(e) => {
                   setPassword(e.target.value);
                 }}
-                className={inputClass}
+                className={inputStyle}
                 type="password"
                 placeholder="Password"
                 autoComplete="current-password"
               />
-              <button className="bg-theme-darkred hover:bg-theme-red focus:bg-theme-red py-3 my-6 rounded font-bold">
+              <button className="bg-theme-red hover:bg-theme-darkred focus:bg-theme-darkred py-3 my-6 rounded font-bold">
                 Sign In
               </button>
-              <div className="flex justify-between items-center text-[0.9rem] text-gray-500">
+              <div className="flex justify-between items-center text-sm text-theme-lightgray">
                 <p>
-                  <input className="mr-[8px]" type="checkbox" />
+                  <input className="mr-2" type="checkbox" />
                   Remember Me?
                 </p>
                 <p className="cursor-pointer">Need Help?</p>
               </div>
               <div>
-                <p className="mt-[16px]">
-                  <span className="text-gray-500 mr-[8px]">
+                <p className="mt-4">
+                  <span className="text-theme-lightgray mr-2">
                     New to Cineflix?
                   </span>{' '}
                   <Link to="/signup">Sign Up</Link>
