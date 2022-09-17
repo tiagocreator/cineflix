0853638789eb3669a3781a2d22ceb14e347788f8
tiagocreator <tiagoleite1405@gmail.com>
2022-09-17 01:04:47 +0000

    Refactor tailwindcss class names

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -38,14 +38,14 @@ const SavedShows = () => {
   };
 
   return (
-    <div className="my-[40px]">
-      <h2 className="text-[#fff] font-bold text-[1.15rem] md:text-[1.30rem] py-[16px] px-[16px] md:px-[32px]">
+    <div className="my-10">
+      <h2 className="font-bold text-lg md:text-xl py-4 px-4 md:px-8">
         Saved Shows
       </h2>
       <div className="relative flex items-center group">
         <BsChevronCompactLeft
           onClick={slideLeft}
-          className="text-[#fff] bg-gradient-to-r from-[#000] opacity-50 hover:opacity-100 absolute left-0 text-[25px] sm:text-[30px] md:text-[40px] lg:text-[50px] z-20 h-[96.92px] sm:h-[119.41px] md:h-[141.88px] lg:h-[164.36px] cursor-pointer hidden group-hover:block group-focus:block group-active:block"
+          className="bg-gradient-to-r from-[#000] opacity-50 hover:opacity-100 absolute left-0 text-[25px] sm:text-[30px] md:text-[40px] lg:text-[50px] z-20 h-[96.92px] sm:h-[119.41px] md:h-[141.88px] lg:h-[164.36px] cursor-pointer hidden group-hover:block group-focus:block group-active:block"
         />
         <div className="absolute left-0 w-[25px] sm:w-[30px] md:w-[40px] lg:w-[50px] z-10 bg-gradient-to-r from-[#000] opacity-50 h-[96.92px] sm:h-[119.41px] md:h-[141.88px] lg:h-[164.36px]"></div>
         <div
@@ -55,22 +55,23 @@ const SavedShows = () => {
           {movies.map((item, id) => (
             <div
               key={id}
-              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative hover:scale-105 mx-[10px] first:ml-[0] last:mr-[0]"
+              className="w-40 sm:w-[200px] md:w-60 lg:w-[280px] inline-block cursor-pointer relative hover:scale-105 mx-2.5 first:ml-0 last:mr-0"
             >
               <img
                 className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
                 alt={item?.title}
               />
-              <div className="absolute top-0 left-0 h-full w-full hover:bg-black/75 opacity-0 hover:opacity-100 text-[#fff]">
-                <p className="whitespace-normal text-[0.75rem] md:text-[0.87rem] font-bold flex justify-center items-center h-full text-center mx-[20%]">
+              <div className="absolute top-0 left-0 h-full w-full hover:bg-black/75 opacity-0 hover:opacity-100">
+                <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center mx-[20%]">
                   {item.title}
                 </p>
                 <p
                   onClick={() => deleteShow(item.id)}
-                  className="absolute top-[16px] right-[16px] text-[#d4d4d4] hover:text-red-600 text-[1.8rem] hover:scale-105"
+                  className="absolute top-4 right-4 text-3xl hover:scale-105"
+                  role="button"
                 >
-                  <FaTimes />
+                  <FaTimes className='text-theme-lightgray hover:text-theme-red'/>
                 </p>
               </div>
             </div>
@@ -78,7 +79,7 @@ const SavedShows = () => {
         </div>
         <BsChevronCompactRight
           onClick={slideRight}
-          className="text-[#fff] bg-gradient-to-l from-[#000] opacity-50 hover:opacity-100 absolute right-0 text-[25px] sm:text-[30px] md:text-[40px] lg:text-[50px] z-20 h-[96.92px] sm:h-[119.41px] md:h-[141.88px] lg:h-[164.36px] cursor-pointer hidden group-hover:block group-focus:block group-active:block"
+          className="bg-gradient-to-l from-[#000] opacity-50 hover:opacity-100 absolute right-0 text-[25px] sm:text-[30px] md:text-[40px] lg:text-[50px] z-20 h-[96.92px] sm:h-[119.41px] md:h-[141.88px] lg:h-[164.36px] cursor-pointer hidden group-hover:block group-focus:block group-active:block"
         />
         <div className="absolute right-0 w-[25px] sm:w-[30px] md:w-[40px] lg:w-[50px] z-10 bg-gradient-to-l from-[#000] opacity-50 h-[96.92px] sm:h-[119.41px] md:h-[141.88px] lg:h-[164.36px]"></div>
       </div>
