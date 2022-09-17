161ddefdafc6e9e913bd0cc0c448284c220c5402
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-17 10:49:16 -0300

    Refactor tailwindcss class names

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -10,7 +10,7 @@ const Account = () => {
           src={BgImg}
           alt=""
         />
-        <div className="bg-black/60 w-full h-[150px] sm:h-[250px] md:h-[300px] z-10"></div>
+        <div className="bg-theme-background-60 w-full h-[150px] sm:h-[250px] md:h-[300px] z-10"></div>
         <div className="h-[150px] sm:h-[250px] md:h-[300px] left-[5%] absolute flex justify-center items-center z-20">
           <h1 className="text-3xl md:text-5xl font-bold">My Account</h1>
         </div>
