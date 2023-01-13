2040cba9c0956542422bd357486adeb79072b3e4
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-13 13:08:27 -0300

    Remove adult movies

--- src/Requests.js
+++ src/Requests.js
@@ -4,11 +4,11 @@ const requests = {
   reqPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   reqTop: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
   reqTrend: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
-  reqAction: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=true&include_video=false&page=1&with_genres=28`,
-  reqAdventure: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=true&include_video=false&page=1&with_genres=12`,
-  reqComedy: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=true&include_video=false&page=1&with_genres=35`,
-  reqHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=true&include_video=false&page=1&with_genres=27`,
-  reqDocumentary: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=true&include_video=false&page=1&with_genres=99`,
+  reqAction: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&include_video=false&page=1&with_genres=28`,
+  reqAdventure: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&include_video=false&page=1&with_genres=12`,
+  reqComedy: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&include_video=false&page=1&with_genres=35`,
+  reqHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&include_video=false&page=1&with_genres=27`,
+  reqDocumentary: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&include_video=false&page=1&with_genres=99`,
 };
 
 export default requests;
