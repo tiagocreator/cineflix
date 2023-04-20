27719ba3e44bd2c77bde2a558efec9499e09a1d7
Tiago Leite <contato@tiagocreator.com>
2023-04-19 23:49:49 -0300

    Refactor auth logic and add custom notifications

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -1,4 +1,6 @@
 import { createContext, useEffect, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { auth, db } from '../firebase';
 import {
   createUserWithEmailAndPassword,
@@ -8,24 +10,73 @@ import {
 } from 'firebase/auth';
 
 import { setDoc, doc } from 'firebase/firestore';
+
+import { useToast } from '../utils/useToast';
+
 const Auth = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const toast = useToast();
 
   const SignUp = async (email, password) => {
-    createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate('/');
+        toast.open('Account created successfully.');
+      })
+      .catch((e) => {
+        if (e.code === 'auth/invalid-email') {
+          toast.open('Please enter a valid email address.');
+        }
+        if (e.code === 'auth/email-already-in-use') {
+          toast.open('Email account is already registered.');
+        }
+        if (e.code === 'auth/weak-password') {
+          toast.open('Password must be at least 6 characters long.');
+        }
+        if (e.code === 'auth/internal-error') {
+          toast.open('Complete all the fields correctly.');
+        }
+      });
     await setDoc(doc(db, 'users', email), {
       savedShows: [],
     });
   };
 
-  const LogIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
+  const LogIn = async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate('/');
+        toast.open('Logged in successfully!');
+      })
+      .catch((e) => {
+        if (e.code === 'auth/invalid-email') {
+          toast.open('Please enter a valid email address.');
+        }
+        if (
+          e.code === 'auth/wrong-password' ||
+          e.code === 'auth/user-not-found' ||
+          e.code === 'auth/internal-error'
+        ) {
+          toast.open('Incorrect email address or password');
+        }
+        if (e.code === 'auth/weak-password') {
+          toast.open('The login password must contain at least 6 characters.');
+        }
+      });
   };
 
   const LogOut = () => {
     window.location.reload(false);
-    return signOut(auth);
+    return signOut(auth)
+      .then(() => {
+        navigate('/');
+        toast.open('Logged out successfully.');
+      })
+      .catch((e) => {
+        toast.open('Error, try again later.');
+      });
   };
 
   useEffect(() => {
