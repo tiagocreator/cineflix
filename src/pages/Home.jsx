f52a437b6d66c71af4a47a06ecd4d42461fbb46f
Tiago Leite <contato@tiagocreator.com>
2023-05-17 21:21:09 -0300

    Add categories slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Intro from '../components/Intro';
 import Row from '../components/Row';
+import CategoriesSlider from '../components/CategoriesSlider';
+
 import Requests from '../Requests.js';
 
 const Home = () => {
@@ -7,23 +9,15 @@ const Home = () => {
     <>
       <main>
         <Intro />
+        <CategoriesSlider />
         <section>
-          <Row id="popular" title="Popular" fetchURL={Requests.reqPopular} />
-          <Row id="top-rated" title="Top Rated" fetchURL={Requests.reqTop} />
-          <Row id="trending" title="Trending" fetchURL={Requests.reqTrend} />
-          <Row id="action" title="Action" fetchURL={Requests.reqAction} />
-          <Row
-            id="adventure"
-            title="Adventure"
-            fetchURL={Requests.reqAdventure}
-          />
-          <Row id="comedy" title="Comedy" fetchURL={Requests.reqComedy} />
-          <Row id="horror" title="Horror" fetchURL={Requests.reqHorror} />
-          <Row
-            id="documentary"
-            title="Documentary"
-            fetchURL={Requests.reqDocumentary}
-          />
+          <Row id='popular' title='Popular' fetchURL={Requests.reqPopular} />
+          <Row id='top-rated' title='Top Rated' fetchURL={Requests.reqTop} />
+          <Row id='trending' title='Trending' fetchURL={Requests.reqTrend} />
+          <Row id='action' title='Action' fetchURL={Requests.reqAction} />
+          <Row id='adventure' title='Adventure' fetchURL={Requests.reqAdventure} />
+          <Row id='comedy' title='Comedy' fetchURL={Requests.reqComedy} />
+          <Row id='horror' title='Horror' fetchURL={Requests.reqHorror} />
         </section>
       </main>
     </>
